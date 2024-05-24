21f26776a5885f361782b36c90ece0b7b3c3fa2f
Krinal09 <krinalgami@gmail.com>
2024-05-24 13:41:55 +0530

    Initial commit

--- main.py
+++ main.py
@@ -75,45 +75,52 @@ def main():
                 gender_result = gender_model.predict(img_process).max()
                 age_result = age_model.predict(img_process).max()
 
-                st.subheader("Your Gender is : {} with Accuracy : {:.2f} %".format(classes[gender], gender_result*100))
-                st.subheader("Your Age Range is : {} years old with Accuracy : {:.2f} %".format(age_classes_reversed[age], age_result*100))
-                
+                st.subheader("Your Gender is : {} ".format(classes[gender], gender_result*100))
+                st.subheader("Your Age Range is : {}".format(age_classes_reversed[age], age_result*100))
+
     if selected == "Camera Detection" :
+        # load model
         gender_model = load_model('Gender_Detection.h5')
         age_model = load_model('Age_Detection.h5')
-        st.title(':blue[REAL TIME AGE AND GENDER DETECTION] :camera:')
-        st.subheader("Click on Start to use webcam and detect your age and gender real time:")
+        st.title(':green[REAL TIME AGE AND GENDER DETECTION] :')
+        st.subheader("Click on Start to use webcam and detect your age and gender real time :heart_decoration:")
         Camera_button = st.button("Start Camera")
         if Camera_button :
-            
+            # Camera frame on streamlit 
             frame_window = st.image([])
             webcam = cv2.VideoCapture(0)
             Stop_camera = st.button("Stop Camera")
             while webcam.isOpened():
-                
+                # read frame from webcam 
                 status, frame = webcam.read()
 
+                # apply face detection
                 face, confidence = cv.detect_face(frame)
 
+                # loop through detected faces
                 for idx, f in enumerate(face):
-       
+
+                    # get corner points of face rectangle        
                     (startX, startY) = f[0], f[1]
                     (endX, endY) = f[2], f[3]
 
+                    # draw rectangle over face
                     cv2.rectangle(frame, (startX,startY), (endX,endY), (0,255,0), 2)
 
+                    # crop the detected face region
                     face_crop = np.copy(frame[startY:endY,startX:endX])
 
                     if (face_crop.shape[0]) < 10 or (face_crop.shape[1]) < 10:
                         continue
 
+                    # preprocessing for gender detection model
                     face_crop = cv2.resize(face_crop, (96,96))
                     face_crop = face_crop.astype("float") / 255.0
                     face_crop = img_to_array(face_crop)
                     face_crop = np.expand_dims(face_crop, axis=0)
 
                     # apply gender detection on face
-                    conf = gender_model.predict(face_crop)[0] 
+                    conf = gender_model.predict(face_crop)[0] # model.predict return a 2D matrix, ex: [[9.9993384e-01 7.4850512e-05]]
                     age_conf = age_model.predict(face_crop)[0]
 
                     # get label with max accuracy
@@ -139,8 +146,8 @@ def main():
 
                     frame_window.image(imgRGB)
 
-                if Stop_camera or cv2.waitKey(1) & 0xFF == ord('q'):
+                if Stop_camera & 0xFF == ord('q'):
                     break
-
+                
 if __name__ == "__main__":
     main()
